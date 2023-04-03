bd45782aa512ad176d82063a24f27f7d27eba471
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-04-03 12:29:08 +0600

    Variable Scope (10)

---
+++ Variable Scope (10)/main.js
@@ -0,0 +1,21 @@
+// function myFun(){
+
+//     var name="ECMAScript6"
+// }
+
+// console.log(name); // Local Scope // (Ata kaj krbe na.. Error asbe)
+
+// function myFun(){
+//     var name="Ash Shams Mim"
+//     console.log(name);
+// }
+// myFun();  // Global Function (ata run hbe )
+
+
+var name="Ash Shams Mim"
+
+function myFun(){
+
+}
+console.log(name); // Global Function (atao run hbe) 
+

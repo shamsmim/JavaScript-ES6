b41a75f36af425539659b466513c61b309f84675
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-04-03 22:35:51 +0600

    For in Loop (16)

---
+++ For in Loop (16)/main.js
@@ -0,0 +1,5 @@
+var billGates={shirt:true,shirtColor:"offwhite",smlie:true,swatter:true,swatterColor:"gray",glass:true}
+
+for(let props in billGates){
+    console.log(props+"="+billGates[props]);
+}

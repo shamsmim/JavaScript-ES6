9592585005901590525272cfceac3c4ab31f7a0d
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-04-03 22:50:14 +0600

    Decision-Making (17)

---
+++ Decision-Making (17)/main.js
@@ -0,0 +1,16 @@
+var billGates={shirt:true,shirtColor:"offwhite",smlie:true,swatter:true,swatterColor:"gray",glass:true}
+
+if(billGates['shirtColor']=="red"){
+    console.log("Shirt Color is Red");
+}
+
+else if(billGates['ahirtColor']=="blue"){
+    console.log("Shirt Color is Blue");
+    
+}
+
+else{
+
+    console.log("Shirt Color Not Found");
+
+}

23ea5ad6c0bd5bde3c9512510079e44782b4c107
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-05-03 21:00:21 +0600

    Multidimensional Array (25)

---
+++ Multidimensional Array (25)/main.js
@@ -0,0 +1,7 @@
+var bangladesh=["Dhaka","Rangpur","Ctg"]
+var india=["Mumbai","Chennai","Kolakata"]
+var china=["Uhan","Shanghai","Honkong"]
+
+var asia=[bangladesh,india,china]
+
+console.log(asia[1][2]);

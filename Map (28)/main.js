9109bb46a0b590bca90c3e05050dcf42d97cc755
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-05-05 19:12:30 +0600

    Map (28)

---
+++ Map (28)/main.js
@@ -0,0 +1,16 @@
+var myMap= new Map();
+
+myMap.set("Key1","Bangladesh")
+myMap.set("Key2","Inaia")
+myMap.set("Key3","Pakistan")
+myMap.set("Key5","Nepal")
+myMap.set("Key6","Japan")
+myMap.set("Key7","China")
+myMap.set("Key8","Australia")
+myMap.set("Key9","Dubai")
+
+for(let mykeys of myMap.keys()){
+    console.log(mykeys);
+}
+
+

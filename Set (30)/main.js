9eba8fab748596cfed7f0da4f6f5e73e6392e164
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-05-12 20:34:35 +0600

    Set (30)

---
+++ Set (30)/main.js
@@ -0,0 +1,13 @@
+var mySet=new Set();
+mySet.add('Bangladesh');
+mySet.add('india');
+mySet.add('Pakistan');
+mySet.add('Australia');
+mySet.add('Canada');
+mySet.add('Japan');
+mySet.add('india');
+mySet.add('india');
+mySet.add('india');
+
+
+console.log(mySet);

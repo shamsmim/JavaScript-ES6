129b3fbe615cb950db876a361eb314aede94f8cd
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-05-13 15:30:04 +0600

    Create class & Use (33)

---
+++ Create class & Use (33)/main.js
@@ -0,0 +1,24 @@
+class MyClass{
+
+    MyFun(){
+        console.log('hello I am from function inside class');
+    }
+    MyFun1(){
+        console.log('hello I am from function inside class');
+    }
+    MyFun2(){
+        console.log('hello I am from function inside class');
+    }
+    MyFun3(){
+        console.log('hello I am from function inside class');
+    }
+
+
+}
+
+var obj=new MyClass;
+
+obj.MyFun();
+obj.MyFun1();
+obj.MyFun2();
+obj.MyFun3();
